5d54cb370dd25d0a16223c9d7bf871867824e3ce
Crazer <nikit.sigov@yandex.ru>
2018-04-23 19:25:47 +0300

    first relize

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-import * as program from 'commander';
 
-program
-  //.description('Usage: gendiff [options] <firstConfig> <secondConfig>')
-  //.description('Compares two configuration files and shows a difference.')
-  //.version('0.0.1')
+import Commander from 'commander';
+
+Commander
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
