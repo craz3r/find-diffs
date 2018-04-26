b645563f6bcb196bb1f40ff734f6152af389ef02
Crazer <nikit.sigov@yandex.ru>
2018-04-27 01:10:51 +0300

    fix task 6

--- src/renders/plain-render.js
+++ src/renders/plain-render.js
@@ -1,26 +1,21 @@
-import _ from 'lodash';
-
-const nodeToString = (prop, node) => {
-  const { value, type } = node;
-  const joinProp = prop.join('');
-
-  const added = typeof value === 'object' ? `Property ${joinProp} was added with complex value\n` : `Property ${joinProp} was added with value: ${value}\n`;
-
-  const strings = {
-    changed: `Property ${joinProp} was updated. From ${typeof value.old === 'object' ? 'complex value' : value.old} to ${typeof value.new === 'object' ? 'complex value' : value.new}\n`,
-    deleted: `Property ${joinProp} was removed\n`,
-    added,
-    immutable: '',
-  };
-  return strings[type];
-};
-
 const render = (ast) => {
   const iter = (astTree, acc) => {
     const keys = Object.keys(astTree);
     return keys.map((key) => {
-      if (_.has(astTree[key], 'children')) return iter(astTree[key].children, [...acc, `${key}.`]);
-      return nodeToString([...acc, `${key}`], astTree[key]);
+      const { value = '', type, children = {} } = astTree[key];
+
+      const joinProp = [...acc, `${key}`].join('');
+      const added = typeof value === 'object' ? `Property ${joinProp} was added with complex value\n` : `Property ${joinProp} was added with value: ${value}\n`;
+
+      const strings = {
+        complex: iter(children, [...acc, `${key}.`]),
+        changed: `Property ${joinProp} was updated. From ${typeof value.old === 'object' ? 'complex value' : value.old} to ${typeof value.new === 'object' ? 'complex value' : value.new}\n`,
+        deleted: `Property ${joinProp} was removed\n`,
+        added,
+        immutable: '',
+      };
+
+      return strings[type];
     }).join('');
   };
 
