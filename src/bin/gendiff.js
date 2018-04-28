b5fbd76da45dc0c065e58686bb05a175eab0b7cd
Sigov Nikita <nikit.sigov@gmail.com>
2018-04-28 16:11:43 +0300

    fix default parameter

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,9 +7,9 @@ Commander
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .version('2.2.1')
-  .option('-f, --format [type]', 'Output format', null, 'diff')
-  .action((file1, file2, options) => {
-    console.log(gendiff(file1, file2, options.format));
+  .option('-f, --format [type]', 'Output format', 'diff')
+  .action((file1, file2) => {
+    console.log(gendiff(file1, file2, Commander.format));
   });
 
 Commander.parse(process.argv);
